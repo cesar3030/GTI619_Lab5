2d4fb00c81395e042e83a5c448b9664b4a0fc1e8
César Jeanroy <cesar.jeanroy@gmail.com>
2015-12-03 12:44:44 -0500

    Commit of the CardGrid Model

---
+++ app/CardGrid.php
@@ -0,0 +1,46 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CardGrid extends Model {
+
+
+ public function getKey(){
+
+        $test=[
+            "A1"=>"58%yu","A2"=>"uy*ty","A3"=>"IHS45","A4"=>"ygu?","A5"=>"iuh$",
+            "B1"=>"58%yu","B2"=>"uy*ty","B3"=>"IHS45","B4"=>"ygu?","B5"=>"iuh$",
+            "C1"=>"58%yu","C2"=>"uy*ty","C3"=>"IHS45","C4"=>"yooou?","C5"=>"iuh$",
+            "D1"=>"58%yu","D2"=>"uy*ty","D3"=>"IHS45","D4"=>"ygu?","D5"=>"iuh$"];
+
+        $letters = array("A","B","C","D");
+
+        $letter = $letters[round(rand(0, (sizeof($letters)-1)))];
+
+        //return $test[$letter.round(rand(1,5))];
+        return $letter.round(rand(1,5));
+ }
+
+
+    /**
+     * @param $key
+     * @param $value
+     * @return string
+     */
+    public function validateValue($key, $value){
+
+        $test=[
+            "A1"=>"58%yu","A2"=>"uy*ty","A3"=>"IHS45","A4"=>"ygu?","A5"=>"iuh$",
+            "B1"=>"58%yu","B2"=>"uy*ty","B3"=>"IHS45","B4"=>"ygu?","B5"=>"iuh$",
+            "C1"=>"58%yu","C2"=>"uy*ty","C3"=>"IHS45","C4"=>"ygu?","C5"=>"iuh$",
+            "D1"=>"58%yu","D2"=>"uy*ty","D3"=>"IHS45","D4"=>"ygu?","D5"=>"iuh$"];
+
+       if($test[$key]==$value){
+           return true;
+       }
+            return false;
+    }
+
+
+
+}
